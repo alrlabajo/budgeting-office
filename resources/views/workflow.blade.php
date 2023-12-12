1deafd5c111c862c175d307bd2b117c1c5a02708
alrlabajo <alrlabajo@gmail.com>
2023-12-12 09:28:58 +0800

    changed dropdown label

--- resources/views/workflow.blade.php
+++ resources/views/workflow.blade.php
@@ -209,7 +209,7 @@
             <!--Completed-->
             <div class="hidden p-4 rounded-lg ml-72 w-[1214px]" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
                 
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black bg-white hover:bg-black hover:text-white border border-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Accounting <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black bg-white hover:bg-black hover:text-white border border-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Workflow <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
                 </button>
